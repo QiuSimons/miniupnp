f173d89d025ed0077894c22ebf03761d507f7e49
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2023-11-30 11:19:22 +0800

    Update upnpevents.c

--- miniupnpd/upnpevents.c
+++ miniupnpd/upnpevents.c
@@ -547,10 +547,10 @@ upnp_event_process_notify(struct upnp_event_notify * obj)
 		}
 		if(err != 0) {
 			errno = err;
-			syslog(LOG_WARNING, "%s: connect(%s%s): %m",
+			/*syslog(LOG_WARNING, "%s: connect(%s%s): %m",
 			       "upnp_event_process_notify",
 			       obj->addrstr, obj->portstr);
-			obj->state = EError;
+			obj->state = EError;*/
 			break;
 		}
 		upnp_event_prepare(obj);
